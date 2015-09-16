641332e869eda9fc2ebca78221b9531474095001
Johan Larsson <johan_gavle1@hotmail.com>
2015-09-16 15:48:25 +0200

    Initial

---
+++ MaslowBox/MainViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MaslowBox
+{
+    public class MainViewModel 
+    {
+        public MainViewModel()
+        {
+            Person = new PersonViewModel {FirstName = "Net", LastName = "Scape"};
+        }
+        public PersonViewModel Person { get; private set; }
+    }
+}
---
+++ MaslowBox/PersonViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using MaslowBox.Annotations;
+
+namespace MaslowBox
+{
+    public class PersonViewModel : INotifyPropertyChanged
+    {
+        private string _firstName;
+        private string _lastName;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                if (value == _firstName) return;
+                _firstName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                if (value == _lastName) return;
+                _lastName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
